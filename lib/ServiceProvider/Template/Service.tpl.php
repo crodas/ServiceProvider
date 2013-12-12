241b89fc0dfd4ca3df01ea62b2cbdc7345d4e2c5
César D. Rodas <crodas@php.net>
2013-12-12 03:44:20 -0300

    Added missing files

---
+++ lib/ServiceProvider/Template/Service.tpl.php
@@ -0,0 +1,29 @@
+@foreach ($service['names'] as $name)
+case {{@$name}}:
+@end
+
+    @if (!empty($service['shared']))
+    if (!empty($services[{{@$name}}])) {
+        return $services[{{@$name}}];
+    }
+    @end
+
+    $config = {{ $self->getConfiguration($service['params']) }};
+
+    @if ($service['object']->isFunction())
+    if (!is_callable({{@$service['object']['function']}})) {
+        require __DIR__ . {{@$service['file']}};
+    }
+    $return = \{{ $service['object']['function'] }}($config, $context);
+    @else
+    if (!class_exists({{@$service['object']['class']}}, false)) {
+        require __DIR__ . {{@$service['file']}};
+    }
+    $object = new \{{$service['object']['class']}};
+    $return = $object->{{$service['object']['function']}}($config, $context);
+    @end
+
+    @if (!empty($service['shared']))
+    $services[{{@$name}}] = $return;
+    @end
+    break;
